116aba0a091ecefeecd488088617b895b93bd3ea
Yeimy Alejandra Rodriguez Ruiz <alejandra.rodriguez@pragma.com.co>
2023-03-24 09:08:34 -0500

    se agrega ejemplo de documentación

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  /// *Aumenta* en 1 la variable [_MyHomePageState._counter] [String].
+  /// 
+  /// __Agregar mas información.__
+  /// ```dart
+  ///   String textoPrueba = 'Hola mundo'.
+  /// ```
+  /// Lista desordenada
+  /// * Opción 1
+  ///   + Opción 2
+  ///     - Opción 3
+  /// 
+  /// Lista ordenada
+  /// 1. Opción 1
+  /// 2. Opción 2
+  /// 3. Opción 3
+  /// 
+  /// Enlaces
+  /// 
+  ///  [enlace]: https://www.google.com/
+  /// 
+  /// * https://www.google.com/
+  /// * [google](https://www.google.com/)
+  /// * [google con referencia][enlace]
+  /// 
+  /// # Titulo 1
+  /// ## Titulo 2
+  /// ### Titulo 3
+  /// #### Titulo 4
+  /// 
+  /// <table>
+  ///   <tr>
+  ///     <td>Celda 1</td>
+  ///     <td>Celda 2</td>
+  ///     <td>Celda 3</td>
+  ///   </tr>
+  ///   <tr>
+  ///     <td>Celda 4</td>
+  ///     <td>Celda 5</td>
+  ///     <td>Celda 6</td>
+  ///   </tr>
+  /// </table>
+  // Este es un comentario
+
+  /*Este
+  es
+  un
+  comentario
+  en bloque
+  */
   void _incrementCounter() {
     setState(() {
       _counter++;
